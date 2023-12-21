b9554a7e6b180bcb61b1661641b624e7c3cb7a2b
dunbar-brett <dunbar.brett@gmail.com>
2023-12-21 12:14:19 -0800

    moved some stuff around. and had to change the versions of the req packages bc things just didn't work with the newer stuff

--- server/app/api/server.py
+++ server/app/api/server.py
@@ -1,16 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
- 
-from app.core import config, tasks  
- 
-from app.api.routes import router as api_router
-import logging
 
-logger = logging.getLogger(__name__)
+from app.api.routes import router as api_router
+from app.core import config, tasks  
+
  
 def get_application():
     app = FastAPI(title=config.PROJECT_NAME, version=config.VERSION)  
- 
+
     app.add_middleware(
         CORSMiddleware,
         allow_origins=["*"],
@@ -18,17 +15,13 @@ def get_application():
         allow_methods=["*"],
         allow_headers=["*"],
     )
- 
-    logger.info("--- APP STARTING ---")
-    logger.info(f"--- Config DB URL {config.DATABASE_URL} ---")
-    logger.info(f"--- Config DB URL {config.POSTGRES_DB} ---")
+
     app.add_event_handler("startup", tasks.create_start_app_handler(app))
     app.add_event_handler("shutdown", tasks.create_stop_app_handler(app))
- 
+
     app.include_router(api_router, prefix="/api")
- 
+
     return app
- 
- 
+
 app = get_application()
  
