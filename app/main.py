28fb1043b2cb1eb01eac8315479a8215ef45fff8
dunbar-brett <dunbar.brett@gmail.com>
2024-01-04 17:04:34 -0800

    added a readme. updated db stuff. seems to work so far. got pg admin to work on docker

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import Settings
 from app.db.session import engine
 from app.db.models.base import Base
+from app.routes import router as api_router
 
 
 origins = [
@@ -15,11 +16,18 @@ origins = [
 
 def create_tables():         
 	Base.metadata.create_all(bind=engine)
-        
+
+def include_router(app):
+    app.include_router(api_router)
+
+
+# def configure_staticfiles(app):
+#     app.mount("/static", StaticFiles(directory="static"), name="static")
 
 def start_application():
     app = FastAPI(title=Settings.PROJECT_NAME,version=Settings.PROJECT_VERSION)
     create_tables()
+    include_router(app)
 
     app.add_middleware(
         CORSMiddleware,
