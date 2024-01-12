100e36c3eddcd7c44a9804dd1b4a41c20992e91e
dunbar-brett <dunbar.brett@gmail.com>
2024-01-11 18:04:59 -0800

    added nearly if not all user routes and repo functions

--- app/db/repos/user.py
+++ app/db/repos/user.py
@@ -5,7 +5,7 @@ from app.db.models.user import User
 from app.schemas.user import UserCreate
 
 
-def create_new_user(user: UserCreate, db: Session):
+def create_user(user: UserCreate, db: Session):
     db_user = User(
         email=user.email,
         hashed_password=Hasher.get_password_hash(user.password),
@@ -17,6 +17,47 @@ def create_new_user(user: UserCreate, db: Session):
     db.refresh(db_user)
     return db_user
 
+# TODO: remove this later
 def list_users(db: Session):
     users = db.query(User).filter(User.is_active == True).all()
     return users
+
+def get_user_by_id(id: int, db: Session):
+    user = db.query(User).filter(User.id == id).first()
+    if not user:
+        return {"error": f"User with id {id} not found"}
+    return user
+
+def get_user_by_email(email: str, db: Session):
+    user = db.query(User).filter(User.email == email).first()
+    if not user:
+        return {"error": f"User with email {email} not found"}
+    return user
+
+def update_user(id: int, user: UserCreate, db: Session):
+    user_email_check = db.query(User).filter(User.email == user.email).first()
+    if user_email_check:
+        return {"error": f"User with email {user.email} already exists"}
+
+    user = db.query(User).filter(User.id == id).first()
+    user.email = user.email
+    user.hashed_password = Hasher.get_password_hash(user.password)
+    db.commit()
+    db.refresh(user)
+    return user
+
+def delete_user(id: int, db: Session):
+    user = get_user_by_id(id=id, db=db)
+    if user.get("error"):
+        return user
+    db.delete(user)
+    db.commit()
+    return user
+
+def deactivate_user(id: int, db: Session):
+    user = get_user_by_id(id=id, db=db)
+    if user.get("error"):
+        return user
+    user.is_active = False
+    db.commit()
+    return user
--- app/routes/user.py
+++ app/routes/user.py
@@ -1,20 +1,62 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
-from app.db.repos.user import create_new_user, list_users
+from app.db.repos.user import (
+    create_user,
+    list_users,
+    get_user_by_id,
+    get_user_by_email,
+    update_user,
+    delete_user
+)
 from app.db.session import get_db
 from app.schemas.user import UserCreate, UserShow
- 
  
 router = APIRouter()
  
  
 @router.post("/", response_model=UserShow, status_code=status.HTTP_201_CREATED)
-def create_user(user: UserCreate, db: Session = Depends(get_db)):
-    user = create_new_user(user=user, db=db)
+def create(user: UserCreate, db: Session = Depends(get_db)):
+    user_with_email = get_user_by_email(email=user.email, db=db)
+    if user_with_email:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email already registered"
+        )
+    user = create_user(user=user, db=db)
     return user
 
 @router.get("/")
-def get_all_users(db: Session = Depends(get_db)):
+def get_all(db: Session = Depends(get_db)):
     users = list_users(db=db)
     return users
+
+@router.get("/{id}", response_model=UserShow)
+def get_by_id(id: int, db: Session = Depends(get_db)):
+    user = get_user_by_id(id=id, db=db)
+    if user.get("error"):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+    return user
+
+@router.put("/{id}", response_model=UserShow, status_code=status.HTTP_202_ACCEPTED)
+def update(id: int, user: UserCreate, db: Session = Depends(get_db)):
+    user = update_user(id=id, user=user, db=db)
+    if user.get("error"):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=user.get("error")
+        )
+    return user
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete(id: int, db: Session = Depends(get_db)):
+    result = delete_user(id=id, db=db)
+    if result.get("error"):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=result.get("error")
+        )
+    return {"msg": f"Deleted User with id {id}"}
